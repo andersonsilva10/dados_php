5b0e921fecc740eb503dc5e1626b2d9450b196e7
Anderson Silva <anderson_silva11000@outlook.com>
2021-12-08 22:56:25 -0300

    Anderson Lista

--- lista.php
+++ lista.php
@@ -11,7 +11,7 @@
       <li class="nav-item" role="presentation">
         <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Lista</button>
       </li>
-      <li class="nav-item" role="presentation">
+      <li class="nav-item " role="presentation">
         <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Lista PHP</button>
       </li>
       <li class="nav-item" role="presentation">
@@ -19,8 +19,72 @@
       </li>
     </ul>
     <div class="tab-content" id="pills-tabContent">
-      <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">Uma Lista é um conjunto de dados dispostos e/ou acessáveis em uma sequência determinada. Um exemplo de lista são lista duplamente ligada (ou lista duplamente encadeada) são lista onde cada elemento tem um ponteiro para o próximo elemento e um ponteiro para o elemento anterior. Desta forma, dado um elemento, podemos acessar ambos os elementos adjacentes: o próximo e o anterior. Se tivermos um ponteiro para o último elemento da lista, podemos percorrer a lista em ordem inversa, bastando acessar continuamente o elemento anterior, até alcançar o primeiro elemento da lista, que não tem elemento anterior.</div>
-      <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab"><img src="imagens/lista.png"></div>
+      <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+        <p class="corfonte"> Uma Lista é um conjunto de dados dispostos e/ou acessáveis em uma sequência determinada. Um exemplo de lista são lista duplamente ligada (ou lista duplamente encadeada) são lista onde cada elemento tem um ponteiro para o próximo elemento e um ponteiro para o elemento anterior. Desta forma, dado um elemento, podemos acessar ambos os elementos adjacentes: o próximo e o anterior. Se tivermos um ponteiro para o último elemento da lista, podemos percorrer a lista em ordem inversa, bastando acessar continuamente o elemento anterior, até alcançar o primeiro elemento da lista, que não tem elemento anterior.
+        </p>
+      </div>
+      <div class="tab-pane fade corfonte" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+        <p>Exemplo 1</p>
+        <code>
+
+        <p> $exemplo1 = array('vermelho', 'azul', 'rosas', 'violetas'); </p>
+        <p> list($cor1, $cor2, $flor1, $flor2) = $exemplo1; </p>
+        <p> echo "$flor1 são $cor1, $flor2 são $cor2."; </p>
+        </code>
+        
+        <?php
+
+        $exemplo1 = array('vermelho', 'azul', 'rosas', 'violetas');
+
+        // Listando todas as variáveis
+        list($cor1, $cor2, $flor1, $flor2) = $exemplo1;
+        echo "$flor1 são $cor1, $flor2 são $cor2.<br>";
+
+
+
+        // utilizando apenas os dois primeiros valores
+        $exemplo2 = array('vermelho', 'azul', 'rosas', 'violetas');
+
+
+        list($cor1, $cor2,,) = $exemplo2;
+        echo "$cor1 é uma cor quente e $cor2 é uma cor frio.";
+
+
+      
+
+        $exemplo3 = array('vermelho', 'azul', 'rosas', 'violetas');
+
+
+        list(,,, $flor2) = $exemplo3;
+        echo "$flor2 são lindas.";
+
+
+
+
+
+        // // Listando apenas alguns deles
+        // list($bebida,, $substancia) = $info;
+        // echo "$bebida tem $substancia.\n";
+
+        // // Ou ignoramos os primeiros valores para conseguir apenas o último 
+        // list(,, $substancia) = $info;
+        // echo "I need $substancia!\n";
+
+        // // list() não funciona com strings
+        // list($bar) = "abcde";
+        // var_dump($bar); // NULL
+        ?>
+
+
+
+
+
+
+
+
+
+
+      </div>
       <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">teste3</div>
     </div>
 
